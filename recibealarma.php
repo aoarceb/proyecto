5bcd0cc09f08c1c45333313d37bd205087436a6f
aoarceb <125314677+aoarceb@users.noreply.github.com>
2023-08-16 09:39:05 -0400

    Create recibealarma.php

---
+++ recibealarma.php
@@ -0,0 +1,15 @@
+<?php
+
+// Function to print out objects / arrays
+function PrintObj ($o) { echo "<pre>"; print_r($o); echo "</pre>"; }
+
+// Load the POST.
+$data = file_get_contents("php://input");
+
+// ...and decode it into a PHP array.
+$data = json_decode($data); 
+
+// Do whatever with the array. 
+
+echo json_encode($data);	;
+?>
